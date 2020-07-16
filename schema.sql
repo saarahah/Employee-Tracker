9a64acccb656edb250060e7f9aa4a1aea48cf8df
saarahah <sarahlferguson07@gmail.com>
2020-07-15 21:13:15 -0400

    tables initial

--- schema.sql
+++ schema.sql
@@ -11,14 +11,21 @@ PRIMARY KEY (id)
 );
 
 CREATE TABLE emprole(
-title VARCHAR (30) NOT NULL,
 id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR (30) NOT NULL,
 salary DECIMAL NOT NULL,
-department_id INTEGER,
+department_id INT NOT NULL,
+PRIMARY KEY (id),
 FOREIGN KEY (department_id)
-PRIMARY KEY (id)
 );
 
 CREATE TABLE employee(
-
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (role_id),
+FOREIGN KEY (manager_id)
 );

0b0d6143bcdb8df6289a7cfdda20084193051797
saarahah <sarahlferguson07@gmail.com>
2020-07-16 23:40:44 -0400

    added view dept

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const { RSA_PKCS1_OAEP_PADDING } = require("constants");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -161,7 +162,7 @@ connection.query("SELECT * FROM emprole", function (err, results){
 
 function viewAllDept(){
 
-    connection.query("SELECT * FROM department", function (error, results){
+    connection.query("SELECT department.id, department.deptname, SUM(emprole.salary) AS utilized_budget FROM employee LEFT JOIN emprole on employee.role_id = emprole.id LEFT JOIN department on emprole.department_id = department.id GROUP BY department.id, department.deptname;", function (error, results){
         if(error) throw error;
         console.table(results);
         back();
@@ -189,7 +190,7 @@ function addDept(){
 
 }
 
-function updateRole(){
+ function updateRole(){
 
     var employees= [];
     connection.query ("SELECT * FROM employee", function(err, results){
@@ -199,16 +200,104 @@ function updateRole(){
         console.log(employee);
         employees.push(employee);
     }
-    inquirer.prompt([{
+    const employeeUp =  inquirer.prompt([
+        {
         name: "update",
-        type: "rawlist",
+        type: "list",
         message: "select travis please give him a bonus",
         choices: employees
         
-    }])
-    })
+    },
+    {
+        name: "role_id",
+        type: "list",
+        message: "what do you want the new role to be?",
+        choices: ["Lead Designer", "Junior Designer", "3D Modeler", "Lead Artist", "Junior Artist", "Manager"]
+    }
+    
+]).then (function(answer){
+    connection.query("UPDATE employee SET ? WHERE ?", 
+    [
+        {
+    
+             role_id: employeeUp.role_id,
+        },
+        {
+        
+             id: 0
+        }
+        
+    ],
+    )
+   
+})
 
+
+    })
+    back();
+    // .then(
+        
+    //     connection.query ("SELECT * FROM emprole", function(err, results){
+            
+    //         var roleArray = [];
+
+    //         for (i=0; i < results.length; i++){
+    //             var role=  results[i].title;
+    //             console.log(role);
+    //             employees.push(role);
+    //         }
+    // inquirer.prompt([
+    //             {
+    //             name: "roleUpdate",
+    //             type: "list",
+    //             message: "select a new role",
+    //             choices: roleArray
+                
+    //         }
+    //         // {
+    //         //     name: "role",
+    //         //     type: "list",
+    //         //     message: "what do you want the new role to be?",
+    //         //     choices: ["Lead Designer", "Junior Designer", "3D Modeler", "Lead Artist", "Junior Artist", "Manager"]
+    //         // }
+    //     ])
+    //     }
+    // )
+    // )
 }
+
+// ]).then (inquirer.prompt(
+//     {
+//         name: "role",
+//         type: "list",
+//         message: "select role",
+//         choices: function(){
+
+//             var empArray=[];
+//             for (i= 0; i< results.length; i++){
+//                 empArray.push(results[i].title);
+//             }
+//             // console.log(empArray);
+//             return empArray;
+//         }
+//     })
+// .then (function(answer){
+//     connection.query("SELECT * FROM emprole", function (err, results){
+
+//     var emproleID;
+//     for(j= 0; j< results.length; j++){
+//         if (results[j].title === answer.role_id){
+//             emproleID = results[j].id;
+
+//     }
+// }
+// })
+//     })
+// })
+// }
+
+
+
 
 function back(){
     inquirer.prompt({
